cf86c7576499508bbbc4def16ad03c784ec6c234
imom0 <mobeiheart@gmail.com>
2013-12-09 07:18:11 +0800

    Add file system loader and app directories loader

--- django_plim/loaders.py
+++ django_plim/loaders.py
@@ -4,10 +4,11 @@
 from functools import partial
 
 from django.template.loader import BaseLoader
-
+from django.template.loaders.app_directories import app_template_dirs
 from django.conf import settings
-from mako.lookup import TemplateLookup
 from django import template as django_template
+
+from mako.lookup import TemplateLookup
 from plim import preprocessor as plim_preprocessor
 from mako.template import Template as MakoTemplate
 
@@ -29,13 +30,25 @@ patch_django_template()
 patch_mako_render()
 
 
-class Loader(BaseLoader):
+class FileSystemLoader(BaseLoader):
     is_usable = True
 
     def load_template_source(self, template_name, template_dirs=None):
         lookup = TemplateLookup(directories=settings.TEMPLATE_DIRS,
                                 preprocessor=plim_preprocessor)
-        return lookup.get_template('index.html'), 'index.html'
+        return lookup.get_template(template_name), template_name
 
     def get_template_sources(self, template_name, template_dirs=None):
-        pass
+        yield
+
+
+class AppDirectoriesLoader(BaseLoader):
+    is_usable = True
+
+    def load_template_source(self, template_name, template_dirs=None):
+        lookup = TemplateLookup(directories=app_template_dirs,
+                                preprocessor=plim_preprocessor)
+        return lookup.get_template(template_name), template_name
+
+    def get_template_sources(self, template_name, template_dirs=None):
+        yield
