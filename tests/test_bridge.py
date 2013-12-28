a1c16cd8eda741614e96b9b4f0e44096ff33f2f8
imom0 <mobeiheart@gmail.com>
2013-12-28 14:39:18 +0800

    Add failed tests for bridge

---
+++ tests/test_bridge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+from unittest import TestCase
+
+
+class BridgeTestCase(TestCase):
+
+    def test_render(self):
+        self.assertFalse(True)
+
+    def test_context(self):
+        self.assertFalse(True)
