4a92b178d6fe2138a70e5f4f9833d7697437561b
imom0 <mobeiheart@gmail.com>
2014-01-14 01:09:03 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='django_plim',
+    version='0.0.1',
+    author='iMom0',
+    author_email='mobeiheart@gmail.com',
+    description=('Introduce plim to django'),
+    license='BSD',
+    keywords='plim mako django slim',
+    url='https://github.com/imom0/django-plim',
+    install_requires=[
+        'plim>=0.9.1',
+    ],
+    test_suite='nose.collector',
+    tests_require='nose==1.3.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
