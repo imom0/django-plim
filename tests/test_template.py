464d06dafd9e804cc1a1f46a68b3a037bddf103f
imom0 <mobeiheart@gmail.com>
2014-01-22 11:46:46 +0800

    Add tests for context

--- tests/test_template.py
+++ tests/test_template.py
@@ -11,3 +11,8 @@ class TemplateTestCase(TestCase):
     def test_template_render(self):
         t = Template('h1 hello')
         self.assertEqual(t.render(), '<h1>hello</h1>')
+
+    def test_context(self):
+        t = Template('h2\n  ${hello}')
+        context = {'hello': u'你好'}
+        self.assertEqual(t.render(**context), u'<h2>你好</h2>')
